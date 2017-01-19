d803d6133c9107c8985e0292d50be29b437ca7e8
eneedham <eneedham@design.upenn.edu>
2017-01-19 09:42:44 -0500

    changing stamen tiles to https

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ zoomControl:false
 });
 
 
-var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{a-d}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png}', {
 attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 subdomains: 'abcd',
 minZoom: 0,
