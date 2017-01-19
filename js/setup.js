90691dfc2883107e9c863a75d798acfa1bfcf495
eneedham <eneedham@design.upenn.edu>
2017-01-19 10:58:49 -0500

    updating basemap attribution to remove http

--- js/setup.js
+++ js/setup.js
@@ -7,7 +7,7 @@ zoomControl:false
 
 
 var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{a-d}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png}', {
-attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+attribution: 'Stamen & OpenStreetMap',
 subdomains: 'abcd',
 minZoom: 0,
 maxZoom: 20,
